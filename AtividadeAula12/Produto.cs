cf97f181c61be4f7e86669df7d5e905d9a789683
NatanaelDp <135743045+NatanaelDp@users.noreply.github.com>
2023-07-25 17:16:47 -0300

    Add files via upload

---
+++ AtividadeAula12/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAula12
+{
+    internal class Produto
+    {
+        public string nome;
+        public string descricao;
+        public int quantidade;
+        public double valor;
+        public bool status;
+
+
+        public void mostra()
+        {
+            Console.WriteLine("Nome:" + nome);
+            Console.WriteLine("Valor " + valor);
+            Console.WriteLine("Status " + status);
+            Console.WriteLine("\nDescrição:" + descricao);
+            
+
+
+        }
+
+        public void desativa()
+        {
+            quantidade= 0;
+            status= false;
+        }
+
+        
+    }
+}
---
+++ AtividadeAula12/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using AtividadeAula12;
+
+Console.WriteLine("Boas-vindas ao Conradito salgados");
+Console.WriteLine("A seguir temos 3 opções de salgados");
+
+
+Produto coxinha = new Produto();
+coxinha.nome = "Coxinha";
+coxinha.valor =  5.50;
+coxinha.quantidade = 10;
+coxinha.descricao = "Nossa suculenta coxinha de frango e catupiry é a melhor da região";
+coxinha.status = true;
+
+coxinha.mostra();
+
+
+Produto esfirra = new Produto();
+
+esfirra.nome = "Esfirra";
+esfirra.valor = 6.00;
+esfirra.quantidade = 10;
+esfirra.descricao = "Nossas esfirras feitas de carne moída , tomate e queijo trazer o sabor da pizza em forma de esfirra!";
+esfirra.status = true;
+
+esfirra.mostra();
+
+Produto kibe= new Produto();
+
+kibe.nome = "Kibe";
+kibe.valor = 7.50;
+kibe.quantidade = 10;
+kibe.descricao = "O Kibe mais saboroso que você poderia comer na vida !";
+kibe.status= true;
+
+kibe.mostra();
+coxinha.desativa();
+esfirra.desativa();
+coxinha.mostra();
+esfirra.mostra();
+
+
+
+Console.ReadKey();
